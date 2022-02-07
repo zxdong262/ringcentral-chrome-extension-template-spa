49446087f45664e06c781ab23c053339eb066ca0
ZHAO Xudong <zxdong@gmail.com>
2022-02-07 15:01:44 +0800

    fix format

--- src/background.js
+++ src/background.js
@@ -1,6 +1,4 @@
 import initBackground from 'ringcentral-embeddable-extension-common/src/spa/background'
-import { thirdPartyConfigs } from 'ringcentral-embeddable-extension-common/src/common/app-config'
-
 /**
  * for background.js, check current tab is extension target tab or not
  * @param {object} tab
